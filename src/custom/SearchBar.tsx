68160a5a56e3388b5d5e4bf6c3db5acc03f31804
yudhaginongpratidina <dev.yudhaginongpratidina@gmail.com>
2025-10-05 15:40:03 +0700

    create search bar

--- src/custom/SearchBar.tsx
+++ src/custom/SearchBar.tsx
@@ -1,9 +1,22 @@
+"use client"
+import { usePathname } from "next/navigation";
 import SearchField from "@/ui/SearchField";
 
 export default function SearchBar() {
+    const pathname = usePathname()
+    
     return (
         <div className="w-full px-2">
-            <SearchField />
+            {pathname.startsWith("/wiki/") && (
+                <form action="">
+                    <SearchField placeholder="Search wiki" />
+                </form>
+            )}
+            {pathname.startsWith("/users/") && (
+                <form action="">
+                    <SearchField placeholder="Search user" />
+                </form>
+            )}
         </div>
     )
 }
